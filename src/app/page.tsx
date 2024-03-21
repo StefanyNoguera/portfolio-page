0f4c597a4c4b389bc1e847fb70a88af4820c22f7
Stefany Noguera <stefanynoguerar@gmail.com>
2024-03-21 17:48:19 +0100

    implemented full page scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,65 @@
 'use client'
 import Image from 'next/image'
 import {Link} from 'react-scroll'
+import { FullPage, Slide } from 'react-full-page';
 
 export default function Home() {
   return (
     <main>
-      <div className="bg-beige h-screen">
-        <div className='flex justify-between pl-3 pr-3'>
-          <div className='relative h-14 w-14 md:h-16 md:w-16'>
-            <Image
-              src="/logo-gray.png"
-              layout="fill"
-              objectFit="cover"
-              alt="Logo"
-            />
-          </div>
-          <div className='flex items-center'>
-            <h5 className='p-4' style={{ cursor: 'pointer' }}>
-              <Link to="about" spy={true} smooth={true} duration={800} onClick={() => {}}>about</Link>
-            </h5>
-            <h5 className='p-4' style={{ cursor: 'pointer' }}>
-              <Link to="services" spy={true} smooth={true} duration={800} onClick={() => {}}>services</Link>
-            </h5>
-            <h5 className='p-4' style={{ cursor: 'pointer' }}>
-              <Link to="my-work" spy={true} smooth={true} duration={800} onClick={() => {}}>my work</Link>
-            </h5>
-            <h5 className='p-4' style={{ cursor: 'pointer' }}>
-              <Link to="contact" spy={true} smooth={true} duration={800} onClick={() => {}}>contact</Link>
-            </h5>
+      <FullPage>
+        <Slide>
+        <div className="bg-beige h-screen">
+          <div className='flex justify-between pl-3 pr-3'>
+            <div className='relative h-14 w-14 md:h-16 md:w-16'>
+              <Image
+                src="/logo-gray.png"
+                layout="fill"
+                objectFit="cover"
+                alt="Logo"
+              />
+            </div>
+            <div className='flex items-center'>
+              <h5 className='p-4' style={{ cursor: 'pointer' }}>
+                <Link to="about" spy={true} smooth={true} duration={800} onClick={() => {}}>about</Link>
+              </h5>
+              <h5 className='p-4' style={{ cursor: 'pointer' }}>
+                <Link to="services" spy={true} smooth={true} duration={800} onClick={() => {}}>services</Link>
+              </h5>
+              <h5 className='p-4' style={{ cursor: 'pointer' }}>
+                <Link to="my-work" spy={true} smooth={true} duration={800} onClick={() => {}}>my work</Link>
+              </h5>
+              <h5 className='p-4' style={{ cursor: 'pointer' }}>
+                <Link to="contact" spy={true} smooth={true} duration={800} onClick={() => {}}>contact</Link>
+              </h5>
+            </div>
           </div>
         </div>
-      </div>
+        </Slide>
 
-      <div className="bg-gray h-screen" id='about'>
+        <Slide>
+        <div className="bg-gray h-screen" id='about'>
 
-      </div>
+        </div>
+        </Slide>
 
-      <div className="bg-beige h-screen" id='services'>
+        <Slide>
+        <div className="bg-beige h-screen" id='services'>
 
-      </div>
+        </div>
+        </Slide>
 
-      <div className="bg-gray h-screen" id='my-work'>
+        <Slide>
+        <div className="bg-gray h-screen" id='my-work'>
 
-      </div>
+        </div>
+        </Slide>
 
-      <div className="bg-beige h-screen" id='contact'>
+        <Slide>
+        <div className="bg-beige h-screen" id='contact'>
 
-      </div>
+        </div>
+        </Slide>
+      </FullPage>
     </main>
   );
 }
