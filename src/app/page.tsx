b56e64c71cefb429227d5959c939a1bb58b3c169
Stefany Noguera <stefanynoguerar@gmail.com>
2024-03-26 17:58:08 +0100

    services page elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,13 @@ export default function Home() {
         </div>
 
         <div className="snap-start bg-beige h-screen" id='services'>
-
+          <h2 className=''>i can help you with</h2>
+          <h4>web development</h4>
+          <p>Crafting dynamic websites and web applications tailored to your needs,
+            using cutting-edge technologies for optimal performance and user experience.</p>
+          <h4>web design</h4>
+          <p>Creating visually stunning and user-friendly interfaces that make a lasting impression,
+            focusing on every detail to ensure an exceptional online presence.</p>
         </div>
 
         <div className="snap-start bg-gray h-screen" id='my-work'>
